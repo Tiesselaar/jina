91ca3ff47395aed3ee5112e3a69fcc4287ef8f2a
Ties Laarakker <tieslaarakker@gmail.com>
2025-10-16 11:51:41 +0200

    fix: westeramstel

--- src/scrapers/jazzAmsterdam/westeramstel.py
+++ src/scrapers/jazzAmsterdam/westeramstel.py
@@ -1,30 +1,37 @@
 from src.tools.scraper_tools import myStrptime, futureDate, makeSoup
+from bs4 import Tag
 
 def formatDate(dateString):
-    dateString = " ".join(dateString.split()) + " 2024"
-    dateFormat = '%d %B %A %Y'
-    date = futureDate(myStrptime(dateString, dateFormat).date())
+    dateFormat = '%A %d %B %Y'
+    date = myStrptime(dateString, dateFormat)
     return date.strftime('%Y-%m-%d')
 
-def getData(event):
-    eventLink = event.select_one('h4.mec-event-title a.mec-color-hover')
-    if "jazz" in eventLink.text.lower():
+
+def format_address(venue):
+    if venue == "Wester-Amstel":
+        return "Amsteldijk Noord 55, 1183 TE Amstelveen"
+    else:
+        raise Exception('Unknown venue')
+
+def getData(event: Tag):
+    info = event.select_one('ul.ha-post-info')
+    date, time, venue = [item.text.strip() for item in info.select('li')]
+    if 'jazz' in event.text.lower():
         eventData = {
-            'date': formatDate(event.select_one('.mec-event-date').text),
-            'time': "15:00",
-            'title': eventLink.text.replace('Sasha Beets', 'Dasha Beets'),
-            'venue': "Wester-Amstel",
-            'price': "€15",
-            'site': eventLink.get('href'),
-            'address': "Amsteldijk Noord 55, 1183 TE Amstelveen"
+            'date': formatDate(date),
+            'time': time.replace(' uur', ''),
+            'title': event.select_one('h4.elementor-heading-title').text.strip(),
+            'venue': venue,
+            'price': "",
+            'site': "https://wester-amstel.nl/agenda/",
+            'address': format_address(venue),
         }
         return eventData
 
 def getEventList():
-    venue_name = 'westeramstel'
     URL = 'https://wester-amstel.nl/agenda/'
-    events = makeSoup(URL).select('.mec-event-list-modern > article.mec-event-article')
-    return events
+    events = makeSoup(URL).select('h4.elementor-heading-title')
+    return [event.parent.parent.parent for event in events]
 
 def bot():
     return map(getData, getEventList())
