84ee6a72ce3342ba2a33643828227558f036a4a9
Ties Laarakker <tieslaarakker@gmail.com>
2025-10-20 07:51:22 +0200

    fix: bau

--- src/scrapers/danceAmsterdam/bau.py
+++ src/scrapers/danceAmsterdam/bau.py
@@ -14,10 +14,10 @@ def getData(event):
     venue, address = formatLocation(event.select_one('p.entry-calendar-event-item-text-location').text)
     eventData = {
         'date': event.select_one('.entry-calendar-event-item-text-datetime time').get('datetime'),
-        'time': event.select('.entry-calendar-event-item-text-datetime time')[1].text,
-        'title': event.select_one('h3.entry-calendar-event-item-text-project').text + \
-            " - " + \
-            event.select_one('h3.entry-calendar-event-item-text-title').text,
+        'time': event.select('.entry-calendar-event-item-text-datetime time')[1].get('datetime'),
+        'title': event.select_one('h3.entry-calendar-event-item-text-project').text,
+            # " - " + \
+            # event.select_one('h3.entry-calendar-event-item-text-title').text,
         'venue': venue,
         'price': "",
         'site': event.select_one('a.flag-target').get('href'),
