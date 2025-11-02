32fff02691ad2c8e11375e98819ecbc505ef9061
Ties Laarakker <tieslaarakker@gmail.com>
2025-11-02 14:09:18 +0100

    fix:melkweg

--- src/scrapers/popAmsterdam/melkweg.py
+++ src/scrapers/popAmsterdam/melkweg.py
@@ -1,7 +1,8 @@
-from src.tools.scraper_tools import myStrptime
-from src.tools.scraper_tools import makeSoup, futureDate
+from src.tools.scraper_tools import makeSoup
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
-CALENDARS = ['popAmsterdam', 'jazzAmsterdam']
+CALENDARS = ['popAmsterdam', 'clubAmsterdam', 'jazzAmsterdam']
 
 def formatPrice(subsoup):
     button_text = subsoup.select_one('div[class^="styles_event-header__content_"]').text
@@ -16,22 +17,31 @@ def formatPrice(subsoup):
         print('no price')
     return ""
 
+def format_datetime(date_time): ## 2025-11-15T20:15:00.000000Z
+    dt = datetime.fromisoformat(date_time.replace("Z", "+00:00"))
+    return dt.astimezone(ZoneInfo("Europe/Amsterdam")).isoformat()
+    
+
 def getData(event):
     site = "https://www.melkweg.nl" + event.select_one('a').get('href')
-    print(site)
     subsoup = makeSoup(site)
+    date_time = format_datetime(subsoup.select_one('time').get("datetime"))
     eventData = {
-        'date': subsoup.select_one('time').get("datetime")[:10],
-        'time': subsoup.select_one('time').get("datetime")[11:16],
+        'date': date_time[:10],
+        'time': date_time.split('T')[1][:5],
         'title': event.select_one('h3[class^="styles_event-compact__title"]').text, 
         'venue': "Melkweg",
         'price': formatPrice(subsoup),
         'site': site,
         'address': "Lijnbaansgracht 234A, 1017 PH Amsterdam"
     }
+    category = getattr(event.select_one('a > div > ul > li > span'), 'text', '').strip()
     if "jazz" in event.text.lower():
         yield {**eventData, 'calendar': 'jazzAmsterdam'}
-    yield {**eventData, 'calendar': 'popAmsterdam'}
+    if category == "Club":
+        yield {**eventData, 'calendar': 'clubAmsterdam'}
+    else:
+        yield {**eventData, 'calendar': 'popAmsterdam'}
 
 
 def getEventList():
