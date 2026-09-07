77e08c944f05357d7cb835e38288f59be6f026b3
Ties <tieslaarakker@gmail.com>
2026-09-07 18:55:51 +0200

    breugem fix

--- src/scrapers/jazzAmsterdam/breugem.py
+++ src/scrapers/jazzAmsterdam/breugem.py
@@ -1,22 +1,29 @@
 from src.tools.scraper_tools import myStrptime
 from src.tools.scraper_tools import makeSoup, futureDate
 
+import re
 
 def formatDate(dateString):
     dateString += " 2024"
     dateFormat = '%A %d %B %H:%M %Y'
     datetime = myStrptime(dateString, dateFormat)
-    date = futureDate(datetime.date(), 60)
-    return date.strftime('%Y-%m-%d'), datetime.strftime('%H:%M') 
+    date = futureDate(datetime.date(), 90)
+    return date.strftime('%Y-%m-%d'), datetime.strftime('%H:%M')
 
 
 def getData(event):
     if ("jazz" in event.text.lower() or "lindy hop" in event.text.lower()):
-        date, time = formatDate(" ".join(event.text.replace("-", "").split()[0:4]))
+        time_element = re.search(r'\d\d:\d\d( tot \d\d:\d\d)?', event.text)
+        if not time_element:
+             return
+        time_element = time_element[0]
+        date_string, title = event.text.split(time_element)
+        date_time_string = ' '.join(date_string.split()[:3]) + ' ' + time_element.split()[0]
+        date, time = formatDate(date_time_string)
         return {
             'date': date,
             'time': time,
-            'title': "-".join(event.text.split('-')[3:]).strip(),
+            'title': title,
             'venue': "Breugem Meeting Point",
             'price': "gratis",
             'site': "https://www.breugemmeetingpoint.nl/agenda",
@@ -25,7 +32,7 @@ def getData(event):
 
 def getEventList():
     url = 'https://www.breugemmeetingpoint.nl/agenda'
-    return makeSoup(url).select('h3.jw-heading-70')
+    return makeSoup(url).select('h3')
 
 def bot():
     return map(getData, getEventList())
